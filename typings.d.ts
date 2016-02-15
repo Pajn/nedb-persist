b765990d2df65fc28ed2818f99c376edfd0bc88b
Rasmus Eneman <pie.or.paj@gmail.com>
2016-02-15 21:16:49 +0100

    fix: Typings to not declare a module

--- typings.d.ts
+++ typings.d.ts
@@ -1,8 +1,6 @@
-declare module 'nedb-persist' {
-  export default function nedb(settings: {filename: string}|{inMemoryOnly: boolean}): {
-    getItem(key, cb: Function),
-    setItem(key, value, cb: Function),
-    removeItem(key, cb: Function),
-    getAllKeys(cb: Function),
-  };
-}
+export default function nedb(settings: {filename: string}|{inMemoryOnly: boolean}): {
+  getItem(key, cb: Function),
+  setItem(key, value, cb: Function),
+  removeItem(key, cb: Function),
+  getAllKeys(cb: Function),
+};
