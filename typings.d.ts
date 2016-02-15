29648b78ed788b51304cc8e079a7504d154b9726
Rasmus Eneman <pie.or.paj@gmail.com>
2016-02-15 15:37:23 +0100

    Improve typings and add it to package.json

--- typings.d.ts
+++ typings.d.ts
@@ -1,8 +1,8 @@
 declare module 'nedb-persist' {
   export default function nedb(settings: {filename: string}|{inMemoryOnly: boolean}): {
-    getItem(key, cb),
-    setItem(key, value, cb),
-    removeItem(key, cb),
-    getAllKeys(cb),
+    getItem(key, cb: Function),
+    setItem(key, value, cb: Function),
+    removeItem(key, cb: Function),
+    getAllKeys(cb: Function),
   };
 }
